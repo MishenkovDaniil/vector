dd7b9e320c2685589d9a9a833b6aeb91707f131a
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-09-09 18:31:41 +0300

    made vector drawing by mouse button click

--- main.cpp
+++ main.cpp
@@ -4,38 +4,46 @@
 #include "vector/vector.h"
 #include "coord_system/coord_system.h"
 
+const int WINDOW_WIDTH  = 800;
+const int WINDOW_HEIGHT = 600;
+
 int main ()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Vectors");// sf::Style::Fullscreen);//, sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Vectors"); //sf::Style::Fullscreen);
     
-    // sf::VertexArray lines(sf::Lines, 4);
-    // lines[0].position = sf::Vector2f(100, 100);
-    // lines[1].position = sf::Vector2f(300, 150);
-    // lines[2].position = sf::Vector2f(700, 550);
-    // lines[3].position = sf::Vector2f(40, 50);
-    // lines[3].color = sf::Color::Blue;
-    Coord_system coord_system (0, 0, 600, 600, 1, 1, sf::VideoMode(800, 600));
+    Coord_system coord_system (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT, 1, 1, sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+    Vector mouse (0, 0, sf::Color::Black);
+
     sf::Color blue_color(sf::Color::Blue);
-    sf::Color red_color(sf::Color::Red);
+    sf::Color red_color (sf::Color::Red);
 
-    Vector vec1 (100, 150, blue_color);
-    Vector vec2 (-50, 100, red_color);
-
+    Vector vec1 (0, 100, blue_color);
+    Vector vec2 (100, 0, red_color);
     Vector vec3 = vec1 + vec2; 
+
     while (window.isOpen())
     {
         sf::Event event;
+        window.clear();
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
                 window.close();
+            else if (event.mouseButton.button == sf::Mouse::Left && (event.mouseButton.x || event.mouseButton.y))
+            {
+               mouse = Vector (event.mouseButton.x - coord_system.x_start_, -(event.mouseButton.y - coord_system.y_start_), sf::Color::Green);
+               break;
+            }
         }
 
-        window.clear();
         coord_system.draw(window);
+        mouse.draw (coord_system, window);
+        
         vec1.draw(coord_system, window);
         vec2.draw(coord_system, window);
         vec3.draw(coord_system, window);
+
         window.display();
     }
 
@@ -43,14 +51,8 @@ int main ()
     vec2.~Vector ();
     vec3.~Vector ();
 
-    // std::cout << vec3.x_coord << std::endl;
-    // std::cout << vec3.y_coord << std::endl;
+    coord_system.~Coord_system ();
+
     return 0;
 }
 
-///to do
-// рисовка окна 
-// функция рисовки плоскости с заданными параметрами 
-// рисовка вектора 
-// кручение вектора 
-// стрелочка на векторе 
