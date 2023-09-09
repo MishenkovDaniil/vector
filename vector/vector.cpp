a7ff2637009aeb61a549fbbad5dda6bf0e9f5e2a
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-09-09 18:30:48 +0300

    add drawing arrow when drawing vector, overloaded operator '=' in vector class

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,7 +1,17 @@
 #include <cmath>
+#include <iostream>
 
 #include "vector.h"
 
+ 
+Point::Point (double x, double y) : 
+        x_(x), y_ (y) {};
+    
+Point::~Point ()
+{
+    this->x_ = NAN;
+    this->y_ = NAN;  
+};
 
 Vector::Vector (double x, double y, sf::Color color) :
     x_coord (x), 
@@ -22,13 +32,56 @@ Vector Vector::operator + (const Vector &vec_a)
     return Vector (this->x_coord + vec_a.x_coord, this->y_coord + vec_a.y_coord, new_color); 
 }
 
-void Vector::draw (Coord_system &coord_system, sf::RenderWindow &window)
+Vector Vector::operator = (const Vector &vec_a)
+{
+    this->x_coord = vec_a.x_coord;
+    this->y_coord = vec_a.y_coord;
+    this->color_ = vec_a.color_;
+    return *this; 
+}
+
+Vector Vector::operator - (const Vector &vec_a)
+{
+    sf::Color new_color ((this->color_.r + vec_a.color_.r) / 2, (this->color_.g + vec_a.color_.g) / 2,
+                        (this->color_.b + vec_a.color_.b) / 2,(this->color_.a + vec_a.color_.a) / 2);
+    return Vector (this->x_coord - vec_a.x_coord, this->y_coord - vec_a.y_coord, new_color); 
+}
+
+Vector Vector::operator - ()
+{
+    return Vector (-this->x_coord, -this->y_coord, this->color_); 
+}
+
+void Vector::draw (const Coord_system &coord_system, sf::RenderWindow &window, const Point start)
+{
+    int len = sqrt (this->y_coord*this->y_coord + this->x_coord*this->x_coord);
+
+    Vector normal   (-(this->y_coord) / (len >> 1), this->x_coord / (len >> 1), this->color_);
+    Vector opposite (  this->x_coord / (len >> 2), this->y_coord / (len >> 2), this->color_);
+    Vector arrow_l = normal - opposite;
+    Vector arrow_r = -normal - opposite;
+
+    Point arrow_start(start.x_ + this->x_coord, start.y_ + this->y_coord);
+
+    draw_line (coord_system, window, start);
+    arrow_l.draw_line (coord_system, window, arrow_start);
+    arrow_r.draw_line (coord_system, window, arrow_start);
+    
+    normal.~Vector ();
+    opposite.~Vector ();
+    arrow_l.~Vector ();
+    arrow_r.~Vector ();
+    arrow_start.~Point ();
+}
+
+void Vector::draw_line (const Coord_system &coord_system, sf::RenderWindow &window, const Point start)
 {
     sf::VertexArray lines(sf::Lines, 2);
 
-    lines[0].position = sf::Vector2f(coord_system.x_start_, coord_system.y_start_);
-    lines[1].position = sf::Vector2f(coord_system.x_start_ + this->x_coord, coord_system.y_start_ + this->y_coord);
-    
+    Point real_start (start.x_ + coord_system.x_start_, -start.y_ + coord_system.y_start_);
+
+    lines[0].position = sf::Vector2f(real_start.x_, real_start.y_);
+    lines[1].position = sf::Vector2f(real_start.x_ + this->x_coord, real_start.y_ - this->y_coord);
     lines[0].color = lines[1].color = this->color_;
 
     window.draw (lines);
--- vector/vector.h
+++ vector/vector.h
@@ -4,10 +4,24 @@
 #include <cmath>
 #include <SFML/Graphics.hpp>
 
+class Coord_system;
 #include "../coord_system/coord_system.h"
+
+class Point 
+{
+public:
+ 
+    double x_ = 0;
+    double y_ = 0;
+
+public:
+    Point (double x, double y);
+    ~Point ();
+};
 
 class Vector 
 {
+    public://
     double x_coord = 0;
     double y_coord = 0;
     sf::Color color_ = sf::Color ();
@@ -16,8 +30,13 @@ public:
     Vector (double x, double y, sf::Color color);
     ~Vector ();
 
-    void draw (Coord_system &coord_system, sf::RenderWindow &window);
+    void draw (const Coord_system &coord_system, sf::RenderWindow &window, const Point start = Point (0, 0));
     Vector operator + (const Vector &vec_a);
+    Vector operator - (const Vector &vec_a);
+    Vector operator = (const Vector &vec_a);
+    Vector operator - ();
+private:
+    void draw_line (const Coord_system &coord_system, sf::RenderWindow &window, const Point start);
 };
 
 
